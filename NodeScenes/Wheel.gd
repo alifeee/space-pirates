d2250907d6ef48ac8619f9830b46a39151e18268
alifeee <alifeeerenn@hotmail.com>
2023-07-08 00:10:45 +0100

    improve wheel controls

--- NodeScenes/Wheel.gd
+++ NodeScenes/Wheel.gd
@@ -4,8 +4,8 @@ var elapsed_reset = 0
 var elapsed_go = 0
 
 const ROTATION_MAX = PI * 0.8
-const CONTROL_SPEED = PI / 10
-const RESET_SPEED = PI / 100
+const CONTROL_SPEED = PI * 5
+const RESET_SPEED = PI * 1
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -15,24 +15,12 @@ func _ready():
 func _process(delta):
 	var joy_amount = Input.get_axis("joy0_left", "joy0_right")
 	
-	var speed	
 	if joy_amount == 0:
-		speed = RESET_SPEED
-		elapsed_go = 0
-	else:
-		speed = CONTROL_SPEED
-		elapsed_reset = 0
-		
-	var start = rotation
-	var end = joy_amount * ROTATION_MAX
-	var weight = (elapsed_go + elapsed_reset) * speed
-	rotation = lerp_angle(start, end, weight)
-	
-	if start - end < 0.01:
-		elapsed_go = 0
-		elapsed_reset = 0
-	
-	elapsed_go += delta
-	elapsed_reset += delta
-	
+		if rotation > 0:
+			rotation = move_toward(rotation, 0, RESET_SPEED * delta)
+		elif rotation < 0:
+			rotation = move_toward(rotation, 0, RESET_SPEED * delta)
+	else: #joystick engaged
+		rotation = move_toward(rotation, joy_amount * ROTATION_MAX, CONTROL_SPEED * delta)
+			
 	pass
