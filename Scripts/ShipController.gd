8b2ce22fe8b935df61aac8a2f4e2dc207b3114e1
DiceMaster <32296289+dicemaster5@users.noreply.github.com>
2023-07-08 10:56:09 +0100

    Asteroids incoming!

--- Scripts/ShipController.gd
+++ Scripts/ShipController.gd
@@ -7,17 +7,24 @@ extends CharacterBody2D
 @export var acceleration = 0.5
 @export var deceleration = 0.5
 
+@onready var _animated_sprite = $AnimatedSprite2D
+
+var shipIsDead = false
 var move_speed = 0
 var coal_count = 0
 var rotation_direction = 0
 
+var collider
+
 func get_input():
+	if(shipIsDead):
+		return
+
 	rotation_direction = Input.get_axis("left", "right")
-	#velocity = -transform.y * Input.get_action_raw_strength("Go!") * move_speed
 	if(Input.is_action_just_pressed("add_coal")):
 		increase_coal(coal_increase_amount)
 
-func _process(delta):
+func _process(_delta):
 	get_input()
 
 func _physics_process(delta):
@@ -30,10 +37,20 @@ func _physics_process(delta):
 		move_speed = move_toward(move_speed, 0, deceleration)
 		move_speed = clamp(move_speed, 0, max_move_speed)
 		velocity = -transform.y * move_speed
-		coal_count = 0;
+		coal_count = 0
 	
 	rotation += rotation_direction * rotation_speed * delta
 	move_and_slide()
 
 func increase_coal(coalAmount):
 	coal_count += coalAmount
+
+func kills_ship():
+	shipIsDead = true
+	_animated_sprite.play("ShipDeath")
+	coal_count = 0
+
+func _on_area_2d_body_entered(body):
+	kills_ship()
+	print("Hit Astroid!!: " + body.name)
+
