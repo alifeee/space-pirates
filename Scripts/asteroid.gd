91a24487e0c9154bba613485fb65122dc8998dca
DiceMaster <32296289+dicemaster5@users.noreply.github.com>
2023-07-08 23:47:15 +0100

    new stuff

--- Scripts/asteroid.gd
+++ Scripts/asteroid.gd
@@ -1,6 +1,7 @@
 extends Area2D
 
 @export var spinSpeed = 1
+@onready var _animated_sprite = $AnimatedSprite2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -10,8 +11,10 @@ func _ready():
 func _process(delta):
 	rotate(spinSpeed * delta)
 
-
 func _on_body_entered(body):
 	if(body.is_in_group("Player")):
+		_animated_sprite.play("explode")
 		body.kills_ship()
 
+func _on_animated_sprite_2d_animation_finished():
+	queue_free()
