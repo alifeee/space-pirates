05ab9d73ced0e3eaa281e754d46b1c69f34cb3cf
alifeee <alifeeerenn@hotmail.com>
2023-07-08 11:41:15 +0100

    rework button code

--- NodeScenes/Button1.gd
+++ NodeScenes/Button1.gd
@@ -6,7 +6,11 @@ var countdown = 0
 const DEFAULT_FRAME = 1
 const PRESSED_FRAME = 0
 
-@export var PRESS_ACTION: String = "joy1_action1"
+@export var PLAYER1_ACTION: String = "joy1_action1"
+@export var PLAYER2_ACTION: String = "joy0_left"
+@export var controller: int = 1
+
+const JOY_THRESHOLD = 0.5
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -14,15 +18,19 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	countdown -= 1
-	if countdown == 0:
-		self.frame = DEFAULT_FRAME
-	pass
+	if controller == 1:
+		var pressed = Input.get_action_raw_strength(PLAYER1_ACTION)
+		if pressed:
+			self.frame = PRESSED_FRAME
+		else:
+			self.frame = DEFAULT_FRAME
+	elif controller == 0:
+		var joy_amount = Input.get_action_strength(PLAYER2_ACTION)
+		if joy_amount > JOY_THRESHOLD:
+			self.frame = PRESSED_FRAME
+		else:
+			self.frame = DEFAULT_FRAME
 
-func _input(event):
-	if event.is_action_pressed(PRESS_ACTION):
-		light_up()
-		
-func light_up():
-	self.frame = PRESSED_FRAME
-	countdown = LIGHT_TIME_FRAMES
+
+
+
