d0c1142e7a46707f917944dcb5e8d5a0aed3b7bd
alifeee <alifeeerenn@hotmail.com>
2023-07-08 21:45:56 +0100

    make oven door drag work!

--- Scripts/CoalOvenDoor.gd
+++ Scripts/CoalOvenDoor.gd
@@ -2,6 +2,11 @@ extends Area2D
 
 @export var door_timer: Timer
 
+const OPEN_HEIGHT = -150
+const CLOSED_HEIGHT = 0
+
+const MIDDLE_OF_DOOR_OFFSET = 100
+
 var open = false
 var locked = false
 
@@ -10,9 +15,11 @@ var dragging = false
 
 signal door_closed
 
+var initial_y_position: int
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	initial_y_position = position.y
 
 func get_controller():
 	return $"..".controller
@@ -30,10 +37,10 @@ func toggle_door():
 func open_door():
 	if locked:
 		return
-	position.y -= 150
+	position.y = initial_y_position + OPEN_HEIGHT
 	open = true
 func close_door():
-	position.y += 150
+	position.y = initial_y_position + CLOSED_HEIGHT
 	door_closed.emit()
 	open = false
 	locked = true
@@ -47,11 +54,18 @@ func start_steering():
 func stop_steering():
 	dragging = false
 	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+	get_viewport().warp_mouse(position + Vector2.DOWN * MIDDLE_OF_DOOR_OFFSET)
+	if position.y < initial_y_position + OPEN_HEIGHT * 0.1:
+		open_door()
+	else:
+		close_door()
 
 func _input(event):
 	if get_controller() != 0:
 		return
 	if event.is_action("joy0_select"):
+		if locked:
+			return
 		if (event.position - self.position).length() and mouse_over_door:
 			if not dragging and event.pressed:
 				start_steering()
@@ -59,8 +73,11 @@ func _input(event):
 			stop_steering()
 
 	if event is InputEventMouseMotion and dragging:
-		var yloc = get_global_mouse_position()
-		print(yloc)
+		if locked:
+			return
+		var yamount = event.relative.y
+		translate(Vector2.DOWN * yamount)
+		position.y = clamp(position.y, initial_y_position + OPEN_HEIGHT, initial_y_position + CLOSED_HEIGHT)
 
 
 func _on_animated_sprite_2d_animation_looped():
