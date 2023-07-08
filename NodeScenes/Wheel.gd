f10b5676551ab309dd23a44423091cbe637c80ac
alifeee <alifeeerenn@hotmail.com>
2023-07-08 11:41:26 +0100

    allow wheel to be controlled by either player

--- NodeScenes/Wheel.gd
+++ NodeScenes/Wheel.gd
@@ -7,20 +7,28 @@ const ROTATION_MAX = PI * 0.8
 const CONTROL_SPEED = PI * 5
 const RESET_SPEED = PI * 1
 
+@export var controller: int = 0
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	var joy_amount = Input.get_axis("joy0_left", "joy0_right")
+	if controller == 0:
+		var joy_amount = Input.get_axis("joy0_left", "joy0_right")
 	
-	if joy_amount == 0:
-		if rotation > 0:
-			rotation = move_toward(rotation, 0, RESET_SPEED * delta)
-		elif rotation < 0:
-			rotation = move_toward(rotation, 0, RESET_SPEED * delta)
-	else: #joystick engaged
-		rotation = move_toward(rotation, joy_amount * ROTATION_MAX, CONTROL_SPEED * delta)
+		var speed
+		if joy_amount == 0:
+			speed = RESET_SPEED
+		else: #joystick engaged
+			speed = CONTROL_SPEED
 			
-	pass
+		rotation = move_toward(rotation, joy_amount * ROTATION_MAX, speed * delta)
+	elif controller == 1:
+		var direction = Input.get_axis("joy1_action1", "joy1_action2")
+		
+		if direction == 1:
+			rotation = move_toward(rotation, ROTATION_MAX, CONTROL_SPEED * delta)
+		elif direction == -1:
+			rotation = move_toward(rotation, -ROTATION_MAX, CONTROL_SPEED * delta)
